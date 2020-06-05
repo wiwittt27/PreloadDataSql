056559acdc40705b495076946ac31c9d8ccef483
alawiyaa <alawi.id05@gmail.com>
2020-06-05 21:40:25 +0700

    add branch

--- app/src/main/java/com/alawiyaa/preloaddata/MahasiswaActivity.kt
+++ app/src/main/java/com/alawiyaa/preloaddata/MahasiswaActivity.kt
@@ -2,11 +2,23 @@ package com.alawiyaa.preloaddata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alawiyaa.preloaddata.database.MahasiswaHelper
+import kotlinx.android.synthetic.main.activity_mahasiswa.*
 
 class MahasiswaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mahasiswa)
+
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        val mahasiswaAdapter = MahasiswaAdapter()
+        recyclerview.adapter = mahasiswaAdapter
+        val mahasiswaHelper = MahasiswaHelper(this)
+        mahasiswaHelper.open()
+        val mahasiswaModels = mahasiswaHelper.getAllData()
+        mahasiswaHelper.close()
+        mahasiswaAdapter.setData(mahasiswaModels)
     }
 }
